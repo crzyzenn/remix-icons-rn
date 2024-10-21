d213ca35bb478158053ad00c663d6c8f4f6eea92
zenishshr <zenish@excitation.com.au>
2024-10-21 10:13:59 +0545

    build: remove dist

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare interface RemixIconProps {
-  name: string;
-  size?: number | string;
-  color?: string;
-  [key: string]: any;
-}
-declare const RemixIcon: React.FC<RemixIconProps>;
-export default RemixIcon;
